1d0f3a157c4f6e8bedc93ee038bbb1898372b764
Andreas <andreas.westh@gmail.com>
2017-08-11 21:11:08 +0200

    tour of heroes meets bootstrap4

--- demonstration/src/app/app.component.ts
+++ demonstration/src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Hero} from './hero';
+
+
+
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+  title = 'Tour of Heroes';
+  title_angular = 'Angular';
   title_bootstrap = 'Bootstrap & ng-bootsrap';
   title_fontawesome = 'Font awesome';
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
--- demonstration/src/app/app.module.ts
+++ demonstration/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
   ],
   imports: [
-    BrowserModule, NgbModule.forRoot()
+    BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
   providers: [],
   bootstrap: [AppComponent]
