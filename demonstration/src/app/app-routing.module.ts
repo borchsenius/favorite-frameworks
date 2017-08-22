9ab81cf631d1df4e047e3cabcec49f965b54479b
Andreas <andreas.westh@gmail.com>
2017-08-22 11:08:12 +0200

    Upgrading to Bootstrap4.0.0-beta and reanabling the old routermodule

--- demonstration/src/app/app-routing.module.ts
+++ demonstration/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import {DemoAngularComponent} from "./demo-angular/demo-angular.component";
+import {DemoBootstrapComponent} from "./demo-bootstrap/demo-bootstrap.component";
+import {DemoFontawesomeComponent} from "./demo-fontawesome/demo-fontawesome.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes',     component: HeroesComponent }
+  { path: 'heroes',     component: HeroesComponent },
+  { path: 'angular',     component: DemoAngularComponent },
+  { path: 'bootstrap',     component: DemoBootstrapComponent },
+  { path: 'fontawesome',     component: DemoFontawesomeComponent }
 ];
 
 @NgModule({
--- demonstration/src/app/app.module.ts
+++ demonstration/src/app/app.module.ts
@@ -15,25 +15,11 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from "./hero.service";
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { RouterModule, Routes } from '@angular/router';
 import { DemoBootstrapComponent } from './demo-bootstrap/demo-bootstrap.component';
 import { DemoFontawesomeComponent } from './demo-fontawesome/demo-fontawesome.component';
 import { DemoAngularComponent } from './demo-angular/demo-angular.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import {AppRoutingModule} from "./app-routing.module";
-
-
-const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard',  component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes',     component: HeroesComponent },
-  { path: 'angular',     component: DemoAngularComponent },
-  { path: 'bootstrap',     component: DemoBootstrapComponent },
-  { path: 'fontawesome',     component: DemoFontawesomeComponent }
-];
-
-
 
 @NgModule({
   declarations: [
